0a6fb3027d23ba2f3e7ef9829137ae96ab403ff6
RizlaneAbalil <38002332@parisnanterre.fr>
2021-09-19 02:11:08 +0200

    modification display data

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, Text } from "react";
+
 
 class App extends Component {
-  state = { users: [] }
+  state = { users: [], clicked: false }
 
   componentDidMount() {
     fetch('/users')
       .then(res => res.json())
       .then(users => this.setState({ users }));
   }
+
+
   render(){
     return (
       <div className="App">
         <h1>Users</h1>
-        <ul>
-          {this.state.users.map(user =>
-            <li key={user.id}>{user.username}</li>
-          )}
-        </ul>
+        <button onClick={() => this.setState({clicked: true})}>Afficher les users</button>
+        {this.state.clicked ?
+            (<ul>
+                {this.state.users.map(user =><li key={user.id}>{user.username}</li>)}
+            </ul>)
+        :
+        null}
       </div>
     );
   }
