1c19a95a721122754032934b7287805c9b43a24c
Raphael Meissonnier <meissonnier.raphael@gmail.com>
2021-09-20 15:22:07 +0200

    Update

--- front-end/src/MyMap.js
+++ front-end/src/MyMap.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import OlMap from "ol/Map";
 import OlView from "ol/View";
+import * as OlLayer from "ol/layer";
+import * as OlSource from "ol/source";
+import * as OlStyle from "ol/style";
+import OlGeomPoint from "ol/geom/Point";
+import OlFeature from "ol/Feature";
 import OlLayerTile from "ol/layer/Tile";
 import OlSourceOSM from "ol/source/OSM";
 import { fromLonLat } from 'ol/proj';
+import marker from './images/marker.png';
+
+var upn_loc=fromLonLat([2.21367,48.9036]);
+var map;
 
 class PublicMap extends Component {
   constructor(props) {
@@ -14,7 +23,7 @@ class PublicMap extends Component {
         zoom: 16 
     };
 
-    this.olmap = new OlMap({
+    map = new OlMap({
       target: null,
       layers: [
         new OlLayerTile({
@@ -29,30 +38,30 @@ class PublicMap extends Component {
   }
 
   updateMap() {
-    this.olmap.getView().setCenter(this.state.center);
-    this.olmap.getView().setZoom(this.state.zoom);
+    map.getView().setCenter(this.state.center);
+    map.getView().setZoom(this.state.zoom);
   }
 
   componentDidMount() {
-    this.olmap.setTarget("map");
+    map.setTarget("map");
 
     // Listen to map changes
-    this.olmap.on("moveend", () => {
-      let center = this.olmap.getView().getCenter();
-      let zoom = this.olmap.getView().getZoom();
+    map.on("moveend", () => {
+      let center = map.getView().getCenter();
+      let zoom = map.getView().getZoom();
       this.setState({ center, zoom });
     });
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    let center = this.olmap.getView().getCenter();
-    let zoom = this.olmap.getView().getZoom();
+    let center = map.getView().getCenter();
+    let zoom = map.getView().getZoom();
     if (center === nextState.center && zoom === nextState.zoom) return false;
     return true;
   }
 
   userAction() {
-    var map = new OlMap({
+    map = new OlMap({
         target: 'map',
         layers: [
           new OlLayerTile({
@@ -60,12 +69,12 @@ class PublicMap extends Component {
           })
         ],
         view: new OlView({
-          center: fromLonLat([2.21367,48.9036]),
+          center: upn_loc ,
           zoom: 20
         })
-      });
+    });
   }
-
+  
   render() {
     this.updateMap(); // Update map on render?
     return (
@@ -74,6 +83,8 @@ class PublicMap extends Component {
       </div>
     );
   }
+  
 }
+
 
 export default PublicMap;
