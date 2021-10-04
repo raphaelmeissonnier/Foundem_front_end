f5c2871492a2723050bac8b39b670478daba995f
Fatimata Soumaré <fati.soumare@outlook.fr>
2021-10-04 16:51:17 +0200

    Initialisation 1.1

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,78 +1,22 @@
-import { numberSafeCompareFunction } from "ol/array";
-import React, { Component, Text } from "react";
+import React from 'react';
+import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import Principale from './Components/principale';
+import Deuxième from './Components/deuxieme';
+import Ajout_objT from './Components/ajout_objT';
 
 
-class App extends React.Component {
+function Monapp() {
+  return(
+    <Router>
+         <Switch>
+          <Route exact path="/" component={Principale}/>
+          <Route path="/deuxième" component={Deuxième}/>
+          <Route path="/ajout_objT" component={Ajout_objT}/>
 
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      latitude: null,
-      longitude: null,
-      userAddress: null,
-    };
-
-    this.getLocation = this.getLocation.bind(this);
-    this.getCoordinates = this.getCoordinates.bind(this)
-  }
-
-  getLocation(){
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.getCoordinates);
-    }
-    else{
-      alert("La géolocalisation n'est pas supportée par votre navigateur");
-    }
-  }
-
-
-  getCoordinates(position) {
-    this.setState({
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
-    })
-   
-  }
-
-  handleLocationError(error) {
-    switch(error.code) {
-      case error.PERMISSION_DENIED:
-        alert("User denied the request for Geolocation.")
-        break;
-      case error.position_UNAVAILABLE:
-        alert("Location information is unavailable.")
-        break;
-      case error.TIMEOUT:
-        alert("The request to get user location timed out.")
-        break;
-      default:
-      alert("An unknow error occurred.")
-    }
-  }
-
-
-
-  render() {
-    return (
-      <div className="App">
-
-        <h2>
-          React Geolocation
-        </h2>
-
-        <button onClick={this.getLocation}> Obtenir coordonnées</button>
-        <h4>Coordonnées de l'utilisateur</h4>
-        <p>Latitude: {this.state.latitude}</p>
-        <p>Longitude: {this.state.longitude}</p>
-        <p>Adresse: {this.state.userAddress}</p>
-      </div>
-    )
-  }
-
-
+       
+         </Switch>
+    </Router>
+  )
 }
 
-
-
-export default App;
+export default Monapp
---
+++ front-end/src/Components/deuxieme.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import Principale from '../Components/principale';
+
+
+
+
+function Deuxieme() {
+  return(
+    <div>
+        test deuxieme
+    </div>
+  )
+}
+
+export default Deuxieme;
