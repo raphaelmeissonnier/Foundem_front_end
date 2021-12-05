9b75e068bf3ec16c9c5da9f81165a5995269e9b7
RizlaneAbalil <38002332@parisnanterre.fr>
2021-12-05 18:26:55 +0100

    merge conflict

--- front-end/src/Routes/Routes.js
+++ front-end/src/Routes/Routes.js
@@ -4,8 +4,8 @@ import { Switch, Route } from "react-router-dom";
 import App from "../App";
 import AjoutObjetTrouve from '../Components/AjoutObjetTrouve';
 import ChercherObjetPerdu from "../Components/ChercherObjetPerdu";
-import AjoutObjetPerdu from '../Components/AjoutObjetPerdu';
-
+import Login from "../Components/Login";
+import Inscription from "../Components/Inscription";
 
 const Routes = () =>{
     return(
@@ -14,11 +14,14 @@ const Routes = () =>{
                 <Route exact path="/" component={App} />
                 <Route exact path="/AjoutObjetTrouve" component={AjoutObjetTrouve} />
                 <Route exact path="/ChercherObjetPerdu" component={ChercherObjetPerdu} />
-                <Route exact path="/AjoutObjetPerdu" component={AjoutObjetPerdu} />
+                <Route exact path="/Inscription" component={Inscription} />
+                <Route exact path="/Login" component={Login} />
+
+
+
             </Switch>
         </div>
     )
 }
-
 
 export default Routes;
