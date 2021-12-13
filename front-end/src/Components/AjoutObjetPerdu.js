3af2c0ea94b00fc968c591c3fb8b9f9ba5f7ea60
RizlaneAbalil <38002332@parisnanterre.fr>
2021-12-13 22:03:09 +0100

    updated form

--- front-end/src/Components/AjoutObjetPerdu.js
+++ front-end/src/Components/AjoutObjetPerdu.js
@@ -1,20 +1,162 @@
-import React, {useState} from 'react';
-import { Paper, Select, Button } from '@material-ui/core';
-import { makeStyles, styled } from '@material-ui/core/styles';
+import React, {useContext, useState} from 'react';
+//import { Paper, Select, Button } from '@material-ui/core';
+//import { makeStyles, styled } from '@material-ui/core/styles';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import './ajoutObjetTrouve.css'
 import Geocoder from "react-mapbox-gl-geocoder"
+
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import {Redirect} from "react-router-dom";
+import {UserContext} from "./UserContext";
+
 const {config} = require('../config');
-
-
 const mapboxApiKey = config.MY_API_TOKEN;
+const params = { country: "fr" };
 
-const params = {
-  country: "fr"
-}
 
 const AjoutObjetPerdu = () => {
 
+    const initialValues = {
+        inititule: "",
+        description: "",
+        date: "",
+        categorie:"",
+        rayon:""
+    };
+
+    const [created, setCreated] = useState(false);
+    const [longitude, setLongitude] = useState(null);
+    const [latitude, setLatitude] = useState(null);
+    const userId = useContext(UserContext);
+
+    const viewport2 = { width: 400, height: 400 };
+
+
+    const validationSchema = Yup.object().shape({
+        intitule: Yup.string().min(3).max(15).required("Veuillez saisir ce champs"),
+        description: Yup.string().max(200),
+        date: Yup.date().required("Veuillez saisir ce champs"),
+        categorie: Yup.string().required("Veuillez saisir ce champs"),
+        rayon: Yup.number().required("Veuillez saisir ce champs"),
+    })
+
+    function onSubmit(values){
+        console.log("On entre dans la fonction onSubmit")
+        if(userId)
+        {
+            //Si au moins un des champs n'est pas saisi
+            if(!values.intitule || !values.date || !values.categorie || !values.rayon || !longitude || !latitude)
+            {
+                console.log("AjoutObjetPerdu.js - Parameters required");
+                return;
+            }
+            console.log("On va lancer le fetch");
+            const requestOptions = {
+                port: 3001,
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify( {intitule: values.intitule, description: values.description, date: values.date, longitude: longitude, latitude: latitude, user_id: userId, categorie: values.categorie, rayon: values.rayon })
+            };
+            fetch('/objetsperdus', requestOptions)
+                //Je récupère la réponse émise par le back
+                .then(response => response.json()
+                    /*Je regarde l'attribut 'result' de la variable 'response'(qui contient la réponse émise par le back)
+                      Si l'attribut 'result'==0 alors je ne fais rien sinon je redirige l'user vers l'accueil + message
+                    */
+                    .then(data => data.result ? (window.alert(data.message), setCreated(true)) : window.alert(data.message)));
+        }
+        else
+        {
+            console.log("AjoutObjetPerdu.js - userId empty");
+        }
+    }
+
+    //Récupération de la longitude et latitude à partir de l'adresse
+    function onSelected(viewport, item){
+        console.log("Item",item.place_name)
+        console.log("Item",item)
+        setLongitude(item.center[0])
+        setLatitude(item.center[1])
+        console.log("Item long",typeof(item.center[0]))
+    }
+
+    return (
+        <div>
+            <br></br><br></br><br></br><br></br><br></br>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}
+            >
+                <Form className="formContainer">
+                    <label>Intitulé:</label>
+                    <ErrorMessage name="intitule" component="span" />
+                    <Field
+                        autoComplete="off"
+                        id="inputCreatePost"
+                        name="intitule"
+                        placeholder="Bonnet noir"
+                    />
+                    <br></br>
+
+                    <label>Description:</label>
+                    <ErrorMessage name="description" component="span" />
+                    <Field
+                        as="textarea"
+                        autoComplete="off"
+                        id="inputCreatePost"
+                        name="description"
+                        placeholder="Bonnet noir de marque Lacoste"
+                    />
+                    <br></br>
+
+                    <label>Catégorie:</label>
+                    <Field as="select" name="categorie">
+                        <option value="Autres">Choisir une catégorie</option>
+                        <option value="PORTEFEUILLE">Portefeuille & CB</option>
+                        <option value="PAPIERS">Papiers & documents officiels</option>
+                        <option value="BAGAGES">Sacs & Bagages</option>
+                        <option value="ELECTRONIQUE">Electronique</option>
+                        <option value="ENFANTS">Affaires d'enfants</option>
+                        <option value="VETEMENTS">Vêtements</option>
+                        <option value="EFFETS PERSONNELS">Effets personnels</option> {/* Effets personnels = clés, lunettes, ...*/}
+                    </Field>
+                    <br></br>
+
+                    <label>Date:</label>
+                    <Field type="date" name="date"/>
+                    <br></br>
+
+                    <label>Adresse:</label>
+                    <Geocoder
+                        mapboxApiAccessToken={mapboxApiKey}
+                        hideOnSelect={true}
+                        onSelected={onSelected}
+                        viewport={viewport2}
+                        updateInputOnSelect={true}
+                        initialInputValue=" "
+                        queryParams={params}
+                    />
+                    <br></br>
+
+                    <label>Rayon:</label>
+                    <Field as="select" name="rayon" placeholder="Choisir un rayon">
+                        <option value="5">5 km</option>
+                        <option value="10">10 km</option>
+                        <option value="15">15 km</option>
+                        <option value="20">20 km</option>
+                    </Field>
+                    <br></br>
+
+                    <button type="submit">Valider</button>
+                </Form>
+            </Formik>
+            {created ? <Redirect to = "/"/> : console.log("not redirect")}
+        </div>
+    );
+
+    /*
   const [intitule, setIntitule] = useState();
   const [description, setDescription] = useState();
   const [date, setDate] = useState();
@@ -24,8 +166,8 @@ const AjoutObjetPerdu = () => {
   const [categorie, setCategorie] = useState();
   const [rayon, setRayon]=useState();
 
-  /* Cette fonction envoyer les infos du formulaire au back
-  */
+  // Cette fonction envoyer les infos du formulaire au back
+
   function envoyerInformations() {
 
     console.log("intitulé envoyé: ", intitule);
@@ -37,9 +179,8 @@ const AjoutObjetPerdu = () => {
     console.log("categorie envoyé: ", categorie);
     console.log("rayon envoyé: ", rayon);
 
-    /*Si un des champs n'a pas été saisi,
-    l'utlisateur est averti par une pop-up
-    */
+    //Si un des champs n'a pas été saisi, l'utlisateur est averti par une pop-up
+
     if(!intitule || !description || !date || !longitude || !latitude || !adresseMail || !categorie || !rayon)
     {
         window.alert("Veuillez saisir tous les champs du formulaire !");
@@ -181,7 +322,7 @@ const classes = useStyles();
         </div>
         <Button onClick={envoyerInformations}>Ajouter</Button>
     </div>
-  )
+  )*/
 }
 
 export default AjoutObjetPerdu;
