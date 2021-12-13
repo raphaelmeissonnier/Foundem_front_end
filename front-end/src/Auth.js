c678097bed577c83a6809edd9244825ab5e174a6
RizlaneAbalil <38002332@parisnanterre.fr>
2021-12-13 09:27:33 +0100

    added comments

--- front-end/src/Auth.js
+++ front-end/src/Auth.js
@@ -10,53 +10,22 @@ const Auth = () => {
     const dispatch = useDispatch();
 
     useEffect(async () => {
-        console.log("On est dans Auth.js");
+        //On vérifie que l'user est connecté
         let response = await fetch('/authId');
-        console.log("On a fait le fetch: ", response);
         let data = await response.json();
         setUserId(data.id);
         console.log("Response /authId: ", data);
 
-        if(data)
+        //Si l'user est connecté alors on récupère ses informations et on les stocke (Redux)
+        if(userId)
         {
-            console.log("userID avant le dispatch: ", userId);
             dispatch(getUser(userId));
         }
         else {
             console.log("data empty");
         }
-        /*const token = async () => {
-            await axios({
-                method: "get",
-                url: backServerURL+`jwtid`,
-                withCredentials: true,
-            })
-                .then((res) => {
-                    setUid(res.data.id);
-                })
-                .catch((err) => {
-                    console.log("No tokens");
-                });
-        };
-        token();
-        dispatch(getallPost());
-        if (uid) {
-            dispatch(getUser(uid));
-            dispatch(getPostTrend(uid));
-        }*/
     }, [userId]);
 
-    /*useEffect( () =>{
-        async function authentication()
-        {
-            console.log("On est dans Auth.js");
-            let response = await fetch("/authId");
-            let data = await response.json;
-            console.log("Fetch terminé - data: ", data);
-            setUserId(data.id);
-        }
-    }, [userId])*/
-    console.log("userId: ", userId);
 
     return (
         <UserContext.Provider value={userId}>
