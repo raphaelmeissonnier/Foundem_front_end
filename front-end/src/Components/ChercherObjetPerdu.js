70f3e12f8cf218a94bbd66e5ce5095bd9daa04f4
RizlaneAbalil <38002332@parisnanterre.fr>
2021-12-20 23:18:45 +0100

    updated form ChercherObjetPerdu

--- front-end/src/Components/ChercherObjetPerdu.js
+++ front-end/src/Components/ChercherObjetPerdu.js
@@ -1,268 +1,243 @@
-import React, {useState, useRef, Label} from 'react';
-import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
-import {InputLabel, Paper, CssBaseline, Container, TextField, Select, Box, Button, IconButton, Grid, Radio, RadioGroup,
-FormControl, FormControlLabel, FormLabel, Fab} from '@material-ui/core';
-import { makeStyles, styled } from '@material-ui/core/styles';
-import NavigationIcon from '@material-ui/icons/Navigation';
-import Accueil from '../Components/Accueil';
-import Header from './Header';
-import Hightech from  '../images/Hightech.png';
-import Livres from  '../images/Livres.png';
-import Garderobe from  '../images/Garderobe.png';
-import Cartes from  '../images/Cartes.png';
-import Beauteetsante from  '../images/Beauteetsante.png';
-import Autres from  '../images/Autres.png';
+import React, {useContext, useState} from 'react';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import './ajoutObjetTrouve.css'
 import Geocoder from "react-mapbox-gl-geocoder"
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import {UserContext} from "./UserContext";
+import * as moment from 'moment';
+import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
 
+const _ = require("lodash");
 const {config} = require('../config');
-
 const mapboxApiKey = config.MY_API_TOKEN;
-
-const params = {
-  country: "fr"
-}
-
-const ChercherObjetPerdu = () => {
-
-  const [intitule, setIntitule] = useState("");
-  const [date, setDate] = useState("");
-  const [longitude, setLongitude] = useState("");
-  const [latitude, setLatitude] = useState("");
-  const [adresseMail, setAdresseMail] = useState("");
-  const [categorie, setCategorie] = useState("");
-  const [items, setItems] = useState([]);
-  const [viewport,setViewport]=useState("");
-  const [idObjet, setIdObjet]=useState(null);
+const params = { country: "fr" };
 
 
-  async function envoyerInformations() {
-    if(!intitule || !date || !longitude || !latitude || !categorie){console.log("intitule:", intitule); return}
-    const requestOptions = {
-        port: 3001,
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({ intitule: intitule, date: date, longitude: longitude, latitude: latitude, categorie: categorie})
-    };
-    let response = await fetch('/objetstrouves/recherche', requestOptions);
-    let data = await response.json();
-    setItems(data);
-  }
+const ChercherObjetPerdu =() =>{
 
-  function afficher()
-  {
-      var renderObjets = [];
-      for(var i =0; i < items.length; i++){
-          renderObjets.push(
-              <div className="list-item" key={items[i][0].id}>
-                  <li>{items[i][0].intitule}</li>
-                  <li>{items[i][0].description}</li>
-                  <li>{items[i][0].categorie}</li>
-                  <li>{items[i][0].date}</li>
-                  {/*<button onClick={() => console.log("Id depuis button: ", items[i][0].id)}>C'est mon objet</button>{/*TOUS LES ID DE PRIS */}
-                  <Link to={{pathname: '/ObjetsMatche/'+items[i][0].id }}><button >Remove</button></Link>
-              </div>
-          );
-      }
-      return renderObjets;
-  }
+    const [longitude, setLongitude] = useState(null);
+    const [latitude, setLatitude] = useState(null);
+    const [items, setItems] = useState([]);
+    const userId = useContext(UserContext);
 
-  function handleRemove(e)
-  {
-      if(e)
-      {
-          setIdObjet(e.target.value);
-      }
-  }
+    const viewport2 = { width: 400, height: 400 };
 
-  function _handleIntituleChange(e){
-    setIntitule(e.target.value);
-    console.log(intitule);
-  }
+    const initialValues = {
+        intitule: "",
+        categorie: "",
+        date: moment(new Date()).format('yyyy-MM-DD'),
+    }
 
-  function _handleDateChange(e){
-    setDate(e.target.value);
-    console.log("date", date);
-  }
+    //Contraintes de validation du formulaire
+    const validationSchema = Yup.object().shape({
+        intitule: Yup.string().min(3, "Trop court").max(15).required("Veuillez saisir ce champs"),
+        date: Yup.date().required("Veuillez saisir ce champs").min(moment().subtract(365, 'days').calendar(), "Date invalide").max(moment(new Date()).format('yyyy-MM-DD'), "Date invalide"),
+        categorie: Yup.string().required("Veuillez saisir ce champs"),
+    })
 
-  function _handleAdresseMailChange(e){
-    setAdresseMail(e.target.value);
-    console.log("adresse mail", adresseMail);
-  }
+    //Récupération de la longitude et latitude à partir de l'adresse
+    function onSelected(viewport, item){
+        console.log("Item",item.place_name)
+        console.log("Item",item)
+        setLongitude(item.center[0])
+        setLatitude(item.center[1])
+        console.log("Item long",typeof(item.center[0]))
+    }
 
-  function _handleCategorieChange(e){
-    setCategorie(e.target.value);
-    console.log("categorie", categorie);
-  }
+    function afficher()
+    {
+        return(
+            <div>
+                <table style={tableStyle}>
+                    <thead>
+                    <tr style={trHoverStyle}>
+                        <th style={thStyle}>Intitulé</th>
+                        <th style={thStyle}>Description</th>
+                        <th style={thStyle}>Catégorie</th>
+                        <th style={thStyle}>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {items.map(item => {
+                        return(
+                            <tr style={trChildStyle} key={item.id}>
+                                <td style={tdStyle}>{_.capitalize(item.intitule)}</td>
+                                <td style={tdStyle}>{_.capitalize(item.description)}</td>
+                                <td style={tdStyle}>{_.capitalize(item.categorie)}</td>
+                                <td style={tdStyle}>{moment(item.date).format("L")}</td>
+                                <td style={tdStyle}><Link to={{pathname: '/ObjetsMatche/'+item.id }}><button>C'est mon objet</button></Link></td>
+                            </tr>
+                        );
+                    })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    /*Some style*/
+    const tableStyle = {
+        fontFamily: "Arial, Helvetica, sans-serif",
+        borderCollapse: "collapse",
+        width: "90%",
+        marginTop: "10px",
+        //center
+        marginLeft: "auto",
+        marginRight: "auto"
+    }
+
+    const tdStyle = {
+        border: "1px solid #ddd",
+        padding: "8px"
+    }
+
+    const thStyle = {
+        paddingTop: "12px",
+        paddingBottom: "12px",
+        textAlign: "left",
+        backgroundColor: "#04AA6D",
+        color: "white",
+        border: "1px solid #ddd",
+        padding: "8px",
+    }
+
+    const trHoverStyle = {
+        backgroundColor: "#ddd"
+    }
+
+    const trChildStyle = {
+        backgroundColor: "#f2f2f2"
+    }
 
 
+    function onSubmit(values){
+        console.log("On entre dans la fonction onSubmit")
+        if(userId)
+        {
+            //Si au moins un des champs n'est pas saisi
+            if(!values.intitule || !values.date || !values.categorie || !longitude || !latitude)
+            {
+                console.log("ChercherObjetPerdu.js - Parameters required");
+                return;
+            }
+            console.log("On va lancer le fetch");
+            const requestOptions = {
+                port: 3001,
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify( {intitule: values.intitule, description: values.description, date: values.date, longitude: longitude, latitude: latitude, user_id: userId, categorie: values.categorie })
+            };
+            fetch('/objetstrouves/recherche', requestOptions)
+                //Je récupère la réponse émise par le back
+                .then(response => response.json()
+                    /*Je regarde l'attribut 'result' de la variable 'response'(qui contient la réponse émise par le back)
+                      Si l'attribut 'result'==0 alors j'affiche un message rien sinon j'enregistre la réponse du back
+                    */
+                    .then(data => (setItems(data), data.result >=0 ? window.alert(data.message) : null )));
+        }
+        else
+        {
+            console.log("ChercherObjetPerdu.js - userId empty");
+        }
+    }
+    console.log("ChercherObjetsPerdus.js - items: ", items);
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-}));
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-          flexGrow: 1,
-          minWidth: 275,
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            flexGrow: 1,
+            minWidth: 275,
         },
-    div: {
-          alignItems: 'center',
-    },
-    grid: {
-          backgroundColor: theme.palette.background.paper,
-          border: '2px solid #000',
-          boxShadow: theme.shadows[5],
-          padding: theme.spacing(2, 4, 3),
-          marginLeft: '70vh',
-          alignItems: 'center',
+        div: {
+            alignItems: 'center',
+        },
+        grid: {
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+            marginLeft: '70vh',
+            alignItems: 'center',
 
         },
-    title: {
-          flexGrow: 1,
-          alignItems: 'center',
-          textAlign: 'center',
+        title: {
+            flexGrow: 1,
+            alignItems: 'center',
+            textAlign: 'center',
 
         },
+    }));
+    const classes = useStyles();
 
-
-}));
-
-function onSelected(viewport, item){
-  setViewport(viewport)
-  console.log("Item",item.place_name)
-  console.log("Item",item)
-  setLongitude(item.center[0])
-  setLatitude(item.center[1])
-  console.log("Item long",typeof(item.center[0]))
-}
-
-const viewport2 = {
-  width: 400,
-  height: 400
-};
-
-const classes = useStyles();
-  return(
-    <div className={classes.div}  >
-        <Header/>
-        <br></br><br></br><br></br><br></br><br></br>
-        <h1 className={classes.title} > Rechercher un objet perdu </h1>
-        <br></br>
-        <center>
-        <p>Merci de renseigner un maximum de champs pour pouvoir vous afficher les articles les plus proches de votre description !</p>
-        </center>
-        <br></br>
-
-        <TextField type="text" onChange={_handleIntituleChange}
-         helperText="Pour trouver votre objet, il nous faut son intitulé !"
-         id="demo-helper-text-aligned"
-         style={{
-          marginLeft: "50px",
-          marginBottom: "20px"
-        }}
-        label="Intitulé"/>
-
-        <br></br>
-
-        <br></br>
-        <div onChange={_handleCategorieChange}>
-                    <input type="radio" value="High-Tech"/> High-Tech
-                    <input type="radio" value="livres" /> Livres
-                    <input type="radio" value="beaute_sante" /> Beauté et santé
-                    <input type="radio" value="Garde-Robe" /> Garde-robe
-                    <input type="radio" value="cartes" /> Cartes
-                    <input type="radio" value="Autres" /> Autres
-        {/*<input type="radio" value="hightech" />
-        <img src={Hightech} width="70" height="70"  style={{marginLeft: "50px", fontSize:"10px"}}/>
-
-        <input type="radio" value="livres" />
-        <img src={Hightech} width="70" height="70"  style={{marginLeft: "50px", fontSize:"10px"}}/>
-
-        <input type="radio" value="beaute_sante" />
-        <img src={Beauteetsante} width="70" height="70" alt="" input type="img" style={{marginLeft: "20px"}} value="Beauteetsante"/>
-
-        <input type="radio" value="garde_robe" />
-        <img src={Garderobe} width="70" height="70" alt="" input type="img" style={{marginLeft: "20px"}} value="Garderobe"/>
-
-        <input type="radio" value="cartes" />
-        <img src={Cartes} width="70" height="70" alt="" input type="img" style={{marginLeft: "20px"}} value="Cartes"/>
-
-        <input type="radio" value="autres" />
-        <img src={Autres} width="70" height="70" alt="" input type="img" style={{marginLeft: "20px"}} value="Autres"/>*/}
-        </div>
-        <br></br>
-        Date: <input type="date" onChange={_handleDateChange}/>
-        <br></br>
-        <br></br>
-        Adresse: <Geocoder
-          mapboxApiAccessToken={mapboxApiKey}
-          hideOnSelect={true}
-          onSelected={onSelected}
-          viewport={viewport2}
-          updateInputOnSelect={true}
-          initialInputValue=" "
-          queryParams={params}
-        />
-        <br></br>
-
-        <br></br>
-
-    <center>
-    <Button onClick={envoyerInformations}
-    style={{
-        borderRadius: 2,
-        backgroundColor: "#5fa082",
-        padding: "5px 20px",
-        fontSize: "15px"
-    }}
-    variant="contained"
-    >
-    Rechercher
-    </Button>
-    </center>
-    <br></br>
-
-       <center>
-        <Button onClick={envoyerInformations}>RECHERCHER</Button>
+    return (
         <div>
-          <table>
-            <thead>
-            <tr>
-              <th>Intitulé</th>
-              <th>Description</th>
-              <th>Catégorie</th>
-              <th>Date</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>test</td>
-              <td>test</td>
-              <td>test</td>
-              <td>test</td>
-            </tr>
-            </tbody>
-          </table>
+            <br></br><br></br><br></br><br></br><br></br>
+
+            <h1 className={classes.title} > Rechercher un objet perdu </h1>
+            <br></br>
+            <center>
+                <p>Merci de renseigner un maximum de champs pour pouvoir vous afficher les articles les plus proches de votre description !</p>
+            </center>
+            <br></br>
+
+            <Formik
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}
+            >
+                <Form className="formContainer">
+                    <label>Intitulé:</label>
+                    <ErrorMessage name="intitule" component="span" />
+                    <Field
+                        autoComplete="off"
+                        id="inputCreatePost"
+                        name="intitule"
+                        placeholder="Bonnet noir"
+                    />
+                    <br></br>
+
+                    {/* Effets personnels = clés, lunettes, ...*/}
+                    <label>Catégorie:</label>
+                    <Field as="select" name="categorie">
+                        <option value="Autres">Choisir une catégorie</option>
+                        <option value="PORTEFEUILLE">Portefeuille & CB</option>
+                        <option value="PAPIERS">Papiers & documents officiels</option>
+                        <option value="BAGAGES">Sacs & Bagages</option>
+                        <option value="ELECTRONIQUE">Electronique</option>
+                        <option value="ENFANTS">Affaires d'enfants</option>
+                        <option value="VETEMENTS">Vêtements</option>
+                        <option value="EFFETS PERSONNELS">Effets personnels</option>
+                    </Field>
+                    <br></br>
+
+                    <label>Date:</label>
+                    <ErrorMessage name="date" component="span" />
+                    <Field type="date" name="date"/>
+                    <br></br>
+
+                    <label>Adresse:</label>
+                    <Geocoder
+                        mapboxApiAccessToken={mapboxApiKey}
+                        hideOnSelect={true}
+                        onSelected={onSelected}
+                        viewport={viewport2}
+                        updateInputOnSelect={true}
+                        initialInputValue=" "
+                        queryParams={params}
+                    />
+                    <br></br>
+
+                    <button type="submit">Rechercher</button>
+                </Form>
+            </Formik>
+
+            <div>
+                {items.length ? afficher() : null}
+            </div>
         </div>
-        </center>
-
-        <div>
-            {items.length ? afficher() : null}
-            {/*{items[0][0].date}*/}
-            {/*{items.map(station => <div key={station}> {station} </div>)}*/}
-        </div>
-
-        {/*{[...items.keys()].map(k => (
-            <li key={k}>items.get(k)</li>
-        ))}*/}
-
-        {/*{items.map((obj) => <li>{obj}</li>)}*/}
-    </div>
-
-  )
-
+    );
 }
+
 
 export default ChercherObjetPerdu;
