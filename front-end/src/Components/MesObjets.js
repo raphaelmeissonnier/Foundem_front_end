3406522055fefd8914bb3a25b8cdf82c9003bdf9
imane <imanemnds@gmail.com>
2021-12-31 20:54:59 +0100

    Update file MesObjets.js

--- front-end/src/Components/MesObjets.js
+++ front-end/src/Components/MesObjets.js
@@ -2,16 +2,30 @@ import React, {useState, useEffect} from 'react';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { Redirect } from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {UserContext} from "./UserContext";
+import {getObjetsPerdus} from "../Actions/UserAction";
+const params = { country: "fr" };
 
 
 const MesObjets = () => {
-
-//const objet = useSelector((state) => state.UserReducer);
+const user = useSelector((state) => state.UserReducer);
 
 
     return (
     <div>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+     <center><p> Salut {user.username} ! Ici, tu peux consulter tous tes objets perdus ou trouvés :) </p>
+
+     <strong><p>{user.username}, tu as perdu tous ces objet. Les voici : </p></strong>
+     </center>
     </div>);
+
+
 
 }
 export default MesObjets;
