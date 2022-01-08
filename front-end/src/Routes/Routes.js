50cbee2ea94f698a471552ff6f0e83a143147ef6
meissonnieraphael <meissonnier.raphael@gmail.com>
2022-01-08 15:18:50 +0100

    Secure Routing in App

--- front-end/src/Routes/Routes.js
+++ front-end/src/Routes/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import {useContext} from "react";
 
 import App from "../App";
 import ChercherObjetPerdu from "../Components/ChercherObjetPerdu";
@@ -9,20 +10,32 @@ import Logout from "../Components/Logout";
 import ObjetsMatche from '../Components/ObjetsMatche';
 import MesObjets from '../Components/MesObjets';
 import AjouterObjet from '../Components/AjouterObjet';
+import {UserContext} from "../Components/UserContext";
 
 const Routes = () =>{
+
+    const userID = useContext(UserContext);
     return(
         <div>
             <Switch>
-                <Route exact path="/" component={App} />
-                <Route exact path="/AjouterObjetPerdu" render={() => <AjouterObjet objet={'perdu'}/>} />
-                <Route exact path="/AjouterObjetTrouve" render={() => <AjouterObjet objet={'trouve'}/>} />
-                <Route exact path="/ObjetsMatche/:idObjet" component={ObjetsMatche} />
-                <Route exact path="/ChercherObjetPerdu" component={ChercherObjetPerdu} />
-                <Route exact path="/Inscription" component={Inscription} />
-                <Route exact path="/Login" component={Login} />
-                <Route exact path="/Logout" component={Logout} />
-                <Route exact path="/MesObjets" component={MesObjets} />
+                {userID ? 
+                <Switch> 
+                    <Route exact path="/" component={App} />
+                    <Route exact path="/Logout" component={Logout} />
+                    <Route exact path="/MesObjets" component={MesObjets} />
+                    <Route exact path="/AjouterObjetPerdu" render={() => <AjouterObjet objet={'perdu'}/>} />
+                    <Route exact path="/AjouterObjetTrouve" render={() => <AjouterObjet objet={'trouve'}/>} />
+                    <Route exact path="/ObjetsMatche/:idObjet" component={ObjetsMatche} />
+                    <Route exact path="/ChercherObjetPerdu" component={ChercherObjetPerdu} /> 
+                </Switch> : 
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route exact path="/Inscription" component={Inscription} />
+                    <Route exact path="/Login" component={Login} />
+                </Switch>
+            }
+                
+               
             </Switch>
         </div>
     )
