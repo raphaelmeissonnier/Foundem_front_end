4532ecdfd40210adf97ab1589c324de45469cd2f
meissonnieraphael <meissonnier.raphael@gmail.com>
2022-01-24 16:23:16 +0100

    few update

--- front-end/src/Actions/MesRdvAction.js
+++ front-end/src/Actions/MesRdvAction.js
@@ -4,7 +4,7 @@ export const GET_RDV = "GET_RDV";
 export const getMesRdv = (uid) => {
     return async (dispatch) => {
         try{
-            let response = await fetch('/users/' + uid +'/rdv');
+            let response = await fetch('/users/'+uid+'/rdv');
             let data = await response.json();
             console.log("MesRdvAction - data: ", data);
             dispatch({type: GET_RDV, payload: data});
