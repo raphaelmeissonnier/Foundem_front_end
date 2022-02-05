c8782d789a63e810bb6f81c031fbad96f560aeab
meissonnieraphael <meissonnier.raphael@gmail.com>
2022-02-05 18:25:10 +0100

    add markers on map

--- front-end/src/MapboxMap/Map.js
+++ front-end/src/MapboxMap/Map.js
@@ -18,7 +18,7 @@ mapboxgl.accessToken = config.MY_API_TOKEN;
 const Map = () => {
 
     const mapContainer = useRef(null);
-    const map = useRef(null);
+    const [map,setMap] = useState(null);
     const [lng, setLng] = useState(2.21);
     const [lat, setLat] = useState(48.90);
     const [longUser, setLongUser] = useState(null);
@@ -44,19 +44,19 @@ const Map = () => {
     
     useEffect(() => {
        
-        if (map.current) return; // initialize map only once
-        else if(longUser && latUser){
-            map.current = new mapboxgl.Map({
+        //if (map) return; // initialize map only once
+        if(longUser && latUser){
+            const map = new mapboxgl.Map({
                 container: mapContainer.current,
                 style: 'mapbox://styles/mapbox/streets-v11',
                 center: [longUser,latUser],
                 zoom: zoom,
                 width: 300
             });
-            map.current.on('move', () => {
-                setLng(map.current.getCenter().lng.toFixed(4));
-                setLat(map.current.getCenter().lat.toFixed(4));
-                setZoom(map.current.getZoom().toFixed(2));
+            map.on('move', () => {
+                setLng(map.getCenter().lng.toFixed(4));
+                setLat(map.getCenter().lat.toFixed(4));
+                setZoom(map.getZoom().toFixed(2));
             });
             const geolocate = new mapboxgl.GeolocateControl({
                 positionOptions: {
@@ -65,7 +65,7 @@ const Map = () => {
                 trackUserLocation: true
             });
             // Add the control to the map.
-            map.current.addControl(geolocate);
+            map.addControl(geolocate);
             // Set an event listener that fires
             // when a geolocate event occurs.
             geolocate.on('geolocate', () => {
@@ -74,23 +74,27 @@ const Map = () => {
                 var long = userlocation.coords.longitude
                 console.log('A geolocate event has occurred.',lati, long);
             });
+            map.on("load", () => {
+                setMap(map); // We declare the map as a State to make it available for every functions.
+                ajoutMarkers(map, items);
+            });
         }
         
                
-    }, [rayon]);
+    }, [changed,rayon]);
 
-    function ajoutMarkers(map,tabObjets){
+    async function ajoutMarkers(map,tabObjets){
         for(var i=0; i< tabObjets.length;i++){
             const el = document.createElement("div");
             el.className = "marker ";
             console.log("Longitude Marker ",tabObjets[i][0].localisation.position.longitude)
             console.log("Latitude Marker ",tabObjets[i][0].localisation.position.latitude)
             new mapboxgl.Marker(el)
-                .setLngLat([tabObjets[i][0].localisation.position.longitude,tabObjets[i][0].localisation.position.longitude])
+                .setLngLat([tabObjets[i][0].localisation.position.longitude,tabObjets[i][0].localisation.position.latitude])
                 .setPopup(
                     new mapboxgl.Popup({ offset: 25 }) // add popups
                         .setHTML(
-                            `<h5><b>Intitulé : ${tabObjets[i][0].intitule}</b></h5>
+                            `<h3><b>Intitulé : ${tabObjets[i][0].intitule}</b></h3>
                             <p>Description : ${tabObjets[i][0].description}</p>
                             <p>Le <b>${tabObjets[i][0].date}</b></p>` 
                         )
@@ -125,12 +129,6 @@ const Map = () => {
         }
     }, [rayon]);
 
-    useEffect( async() =>{
-        console.log("2ème UseEffect - On vide items2");
-        await ajoutMarkers(map.current,items)
-  
-    }, [items])
-
     /*function _handleRayonChange(e)
     {
         setRayon(e.target.value);
@@ -145,7 +143,7 @@ const Map = () => {
             <div className="sidebar">
             Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
             </div>
-          <div style={{height:'400px', width:'800px'}}className="map-container" ref={mapContainer} />
+            <div style={{height:'400px', width:'800px'}}className="map-container" ref={mapContainer} />
             {/*<div>
                 <Stack direction="row" spacing={2} >
                     <Item elevation={8} style={{width:'70%', marginLeft:'10px', marginRight:'10px', marginBottom:'10px', marginTop:'10px', textAlign:'center'}}>
