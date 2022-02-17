eeb9465f8e7ff71d62c03ea67a1ba3efc5f16f5c
RizlaneAbalil <38002332@parisnanterre.fr>
2022-02-18 00:01:53 +0100

    notifications

--- front-end/src/Components/Authentification/Login.js
+++ front-end/src/Components/Authentification/Login.js
@@ -2,18 +2,29 @@ import React, {useState} from 'react';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Redirect } from "react-router-dom";
 import i18n from "../../Translation/i18n";
+import './style.css';
+import "bootstrap/dist/css/bootstrap.css";
+import {Snackbar} from "@material-ui/core";
+import {Alert} from "@mui/material";
 
 
 const Login =() =>{
 
-   const [iscreated, setcreated] = useState(false);
+    const [iscreated, setcreated] = useState(false);
+    const [openError, setOpenError] = useState(false);
+    const [state, setState] = useState({
+        vertical: 'top',
+        horizontal: 'center',
+    });
+    const { vertical, horizontal} = state;
 
-   const initialValues = {
+
+    const initialValues = {
           username: "",
           password: "",
       };
 
-  function onSubmit(values){
+    function onSubmit(values){
       //Si au moins un des champs n'est pas saisi
       if(!values.username || !values.password)
       {
@@ -34,40 +45,62 @@ const Login =() =>{
                 Si l'attribut 'result'==0 alors je ne fais rien sinon je redirige l'user vers l'accueil
               */
               .then(data => data.result ? setcreated(true): window.alert(data.msg)));
-  }
+    }
 
     return (
         <div>
-            <br></br><br></br><br></br><br></br><br></br>
             <Formik
              initialValues={initialValues}
              onSubmit={onSubmit}
             >
-                <Form className="formContainer">
-                    <label>{i18n.t('inscription.username')}</label>
-                    <ErrorMessage name="username" component="span" />
-                    <Field
-                    autoComplete="off"
-                    id="inputCreatePost"
-                    name="username"
-                    placeholder="Votre pseudo"
-                    />
+                <div className="registration-form">
+                    <Form className="formContainer">
+                        <div className="title">
+                            <h3>{i18n.t('connexion.login')}</h3>
+                        </div>
 
-                    <label>{i18n.t('inscription.password')}</label>
-                    <ErrorMessage name="password" component="span" />
-                    <Field
-                        autoComplete="off"
-                        type="password"
-                        id="inputCreatePost"
-                        name="password"
-                        placeholder="Votre mot de passe"
-                    />
+                        <div className="form-group">
+                            <ErrorMessage name="username" component="span" className="text-danger" />
+                            <Field
+                                className="form-control item"
+                                autoComplete="off"
+                                id="inputCreatePost"
+                                name="username"
+                                placeholder="Votre pseudo"
+                            />
+                        </div>
 
-                    <button type="submit">{i18n.t('connexion.login')}</button>
-                </Form>
+                        <div className="form-group">
+                            <ErrorMessage name="password" component="span" className="text-danger" />
+                            <Field
+                                className="form-control item"
+                                autoComplete="off"
+                                type="password"
+                                id="inputCreatePost"
+                                name="password"
+                                placeholder="Votre mot de passe"
+                            />
+                        </div>
+
+                        <center>
+                            <div className="form-group mb-3">
+                                <button type="submit" className="btn btn-block create-account">{i18n.t('connexion.login')}</button>
+                            </div>
+                        </center>
+
+                    </Form>
+                </div>
             </Formik>
 
-            {iscreated ? <Redirect to = "/"/> : console.log("User not logged")}
+            {iscreated ?
+                <Redirect to = "/"/>
+                :
+                <Snackbar anchorOrigin={{ vertical, horizontal }} open={openError} autoHideDuration={2000} onClose={() => setOpenError(false)}>
+                    <Alert severity="error" variant="filled" sx={{ width: '100%' }}>
+                        {i18n.t("inscription.errorRegistration")}
+                    </Alert>
+                </Snackbar>
+            }
 
         </div>
 
--- front-end/src/Components/Static/Header.js
+++ front-end/src/Components/Static/Header.js
@@ -1,46 +1,50 @@
-import React from 'react';
-import {AppBar, Box, Toolbar, Typography, Menu, MenuItem, IconButton, Avatar} from '@material-ui/core';
+import React, {useContext, useEffect, useState} from 'react';
+import {AppBar, Box, Toolbar, Typography, Menu, MenuItem, IconButton, Avatar, Snackbar} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import Container from '@mui/material/Container';
 import Tooltip from '@mui/material/Tooltip';
 import Button from '@mui/material/Button';
 import imageAvatar from '../../images/Cartes.png';
 import { Link } from 'react-router-dom';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import logo from '../../images/logo.jpg'
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Badge from '@mui/material/Badge';
 import i18 from "../../Translation/i18n";
-<<<<<<< Updated upstream
-=======
 import {UserContext} from "../Authentification/UserContext";
 import {getUser, getAllRdv} from "../../Actions/UserAction";
->>>>>>> Stashed changes
 
 var _ = require('lodash');
 
 const Header = () =>{
 
-
-<<<<<<< Updated upstream
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const userData = useSelector((state) => state.UserReducer);
-=======
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
+  const [anchorElNotif, setAnchorElNotif] = useState(null);
   const [username, setUsername] = useState(null);
-  const count= useSelector((state) => state.UserReducer.getAllRdv);
+  const countResponse = useSelector((state) => state.UserReducer.getAllRdvResponse);
+  const [count, setCount] = useState(null);
   const userID = useContext(UserContext);
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.UserReducer.getUserResponse);
 
   useEffect( async () => {
-    dispatch(getUser(userID));
-    dispatch(getAllRdv(userID));
+    if(userID) {
+      dispatch(getUser(userID));
+      dispatch(getAllRdv(userID));
+    }
   }, [userID])
->>>>>>> Stashed changes
+
+  useEffect(async () => {
+    if(userData)
+    {
+      setUsername(userData.username);
+    }
+    if(countResponse)
+    {
+      setCount(countResponse.nbRdv);
+    }
+  })
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -57,154 +61,188 @@ const Header = () =>{
     setAnchorElUser(null);
   };
 
+  const handleOpenNotificationsMenu = (event) => {
+    setAnchorElNotif(event.currentTarget);
+  }
+
+  const handleCloseNotificationsMenu = () => {
+    setAnchorElNotif(null);
+  }
+
   return(
-    <AppBar position="static">
-      <Container maxWidth="xl" style={{flexDirection:'row'}}>
-        <Toolbar disableGutters>
-        <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              href="/"
-              color="inherit"
-            >
-          <img src={logo} alt="logo"/>
-          </IconButton>
-
-          {!_.isEmpty(userData) ?
-              <div>
-            <Box sx={{ paddingLeft: 10, flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+      <AppBar position="static">
+        <Container maxWidth="xl" style={{flexDirection:'row'}}>
+          <Toolbar disableGutters>
             <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                href="/"
+                color="inherit"
             >
-              <MenuIcon />
+              <img src={logo} alt="logo"/>
             </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-                <MenuItem component={Link} to={"/AjouterObjetPerdu"} >
-                  <Typography textAlign="center">{i18.t('header.lostItem')}</Typography>
-                </MenuItem>
-                <MenuItem component={Link} to={"/AjouterObjetTrouve"}>
-                  <Typography textAlign="center">{i18.t('header.foundItem')}</Typography>
-                </MenuItem>
-                <MenuItem component={Link} to={"/ChercherObjetPerdu"}>
-                  <Typography textAlign="center">{i18.t('header.searchItem')}</Typography>
-                </MenuItem>
-            </Menu>
-          </Box>
-          <Box sx={{ paddingLeft:20, flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              <Button href="/AjouterObjetPerdu" onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
-                {i18.t('header.lostItem')}
-              </Button>
-              <Button href="/AjouterObjetTrouve" onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
-                {i18.t('header.foundItem')}
-              </Button>
-              <Button href="/ChercherObjetPerdu" onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
-                {i18.t('header.searchItem')}
-              </Button>
-          </Box>
-              </div>
-              : <Box sx={{ paddingLeft:20, flexGrow: 1, display: { xs: 'none', md: 'flex' }, width:'55%' }}/>}
 
-          <Box sx={{textAlign:'right', width:'40%'}}>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={count} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <Tooltip title="Profil">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt={userData.username}  src={imageAvatar} />
-              </IconButton>
-            </Tooltip>
-            
-            
-              {!_.isEmpty(userData) ? 
-              <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-                <MenuItem  component={Link} to={"/MesObjets"}>
-                  <Typography textAlign="center">{i18.t('header.myItems')}</Typography>
-                </MenuItem>
-                <MenuItem  component={Link} to={"/MesRdv"}>
-                  <Typography textAlign="center">{i18.t('header.myRdv')}</Typography>
-                </MenuItem>
-                <MenuItem  component={Link} to={"/MonSolde"}>
-                  <Typography textAlign="center">{i18.t('header.myBalance')}</Typography>
-                </MenuItem>
-                <MenuItem  component={Link} to={"/Logout"}>
-                  <Typography textAlign="center">{i18.t('header.logout')}</Typography>
-                </MenuItem>
-                </Menu>
-                :
-                <Menu
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-                <MenuItem  component={Link} to={"/Inscription"}>
-                  <Typography textAlign="center">{i18.t('header.registration')}</Typography>
-                </MenuItem>
-                <MenuItem  component={Link} to={"/Login"}>
-                  <Typography textAlign="center">{i18.t('header.login')}</Typography>
-                </MenuItem>
-                </Menu>
+            {!_.isEmpty(userData) ?
+                <div>
+                  <Box sx={{ paddingLeft: 10, flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleOpenNavMenu}
+                        color="inherit"
+                    >
+                      <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                          vertical: 'bottom',
+                          horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                          vertical: 'top',
+                          horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                          display: { xs: 'block', md: 'none' },
+                        }}
+                    >
+                      <MenuItem component={Link} to={"/AjouterObjetPerdu"} >
+                        <Typography textAlign="center">{i18.t('header.lostItem')}</Typography>
+                      </MenuItem>
+                      <MenuItem component={Link} to={"/AjouterObjetTrouve"}>
+                        <Typography textAlign="center">{i18.t('header.foundItem')}</Typography>
+                      </MenuItem>
+                      <MenuItem component={Link} to={"/ChercherObjetPerdu"}>
+                        <Typography textAlign="center">{i18.t('header.searchItem')}</Typography>
+                      </MenuItem>
+                    </Menu>
+                  </Box>
+                  <Box sx={{ paddingLeft:20, flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    <Button href="/AjouterObjetPerdu" onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
+                      {i18.t('header.lostItem')}
+                    </Button>
+                    <Button href="/AjouterObjetTrouve" onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
+                      {i18.t('header.foundItem')}
+                    </Button>
+                    <Button href="/ChercherObjetPerdu" onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
+                      {i18.t('header.searchItem')}
+                    </Button>
+                  </Box>
+                </div>
+                : <Box sx={{ paddingLeft:20, flexGrow: 1, display: { xs: 'none', md: 'flex' }, width:'55%' }}/>}
+
+            <Box sx={{textAlign:'right', width:'40%'}}>
+              {!_.isEmpty(userData) ?
+                  <Tooltip title="Notifications">
+                  <IconButton
+                      size="large"
+                      aria-label="show 17 new notifications"
+                      color="inherit"
+                      onClick={handleOpenNotificationsMenu}
+                  >
+                    <Badge badgeContent={count} color="error">
+                      <NotificationsIcon/>
+                    </Badge>
+                  </IconButton>
+                  </Tooltip>
+                : null
               }
-            
-          </Box>
-          <h4>{userData.username}</h4>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              {!_.isEmpty(userData) ?
+                  <Menu
+                      sx={{ mt: '45px' }}
+                      id="menu-appbar"
+                      anchorEl={anchorElNotif}
+                      anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      open={Boolean(anchorElNotif)}
+                      onClose={handleCloseNotificationsMenu}
+                  >
+                    <Typography>Ceci est la notif</Typography>
+                  </Menu> : null
+              }
+              <Tooltip title="Profil">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt={username}  src={imageAvatar} />
+                </IconButton>
+              </Tooltip>
+
+
+              {!_.isEmpty(userData) ?
+                  <Menu
+                      sx={{ mt: '45px' }}
+                      id="menu-appbar"
+                      anchorEl={anchorElUser}
+                      anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      open={Boolean(anchorElUser)}
+                      onClose={handleCloseUserMenu}
+                  >
+                    <MenuItem  component={Link} to={"/MesObjets"}>
+                      <Typography textAlign="center">{i18.t('header.myItems')}</Typography>
+                    </MenuItem>
+                    <MenuItem  component={Link} to={"/MesRdv"}>
+                      <Typography textAlign="center">{i18.t('header.myRdv')}</Typography>
+                    </MenuItem>
+                    <MenuItem  component={Link} to={"/MonSolde"}>
+                      <Typography textAlign="center">{i18.t('header.myBalance')}</Typography>
+                    </MenuItem>
+                    <MenuItem  component={Link} to={"/Logout"}>
+                      <Typography textAlign="center">{i18.t('header.logout')}</Typography>
+                    </MenuItem>
+                  </Menu>
+                  :
+                  <Menu
+                      id="menu-appbar"
+                      anchorEl={anchorElUser}
+                      anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      open={Boolean(anchorElUser)}
+                      onClose={handleCloseUserMenu}
+                  >
+                    <MenuItem  component={Link} to={"/Inscription"}>
+                      <Typography textAlign="center">{i18.t('header.registration')}</Typography>
+                    </MenuItem>
+                    <MenuItem  component={Link} to={"/Login"}>
+                      <Typography textAlign="center">{i18.t('header.login')}</Typography>
+                    </MenuItem>
+                  </Menu>
+              }
+
+            </Box>
+            <h4>{username}</h4>
+          </Toolbar>
+        </Container>
+      </AppBar>
   )
 }
 export default Header;
